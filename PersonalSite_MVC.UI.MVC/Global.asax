e5efde627da9700832d2b49e44e220441fcfe5eb
Jeremy Sidener <eighthstreettaproom@gmail.com>
2021-02-04 17:36:51 -0600

    1st Commit

---
+++ PersonalSite_MVC.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalSite_MVC.UI.MVC.MvcApplication" Language="C#" %>
---
+++ PersonalSite_MVC.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalSite_MVC.UI.MVC.Startup))]
+namespace PersonalSite_MVC.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
